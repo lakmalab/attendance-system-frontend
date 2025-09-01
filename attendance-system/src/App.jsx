9600dbc3228c08d9779cafe2c28177708de431f1
lakma <lakmalaberathne@gmail.com>
2025-09-01 19:10:15 +0530

    initial commit

---
+++ attendance-system/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Hello Lakmal,</h1>
+      <HelloLakmal />
+    </div>
+    
+    </>
+  )
+}
+export function HelloLakmal(){
+  return(
+    <>
+      <h2> Welcome to React</h2>
+    </>
+  )
+}
+export default App
